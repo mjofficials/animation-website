f648c1f7a46f1d62a54d4bc0e6b7217f72866e2d
mjofficials <manojmarimuthu1995@gmail.com>
2021-02-02 00:59:30 +0530

    added animation to navbar

--- script.js
+++ script.js
@@ -2,11 +2,20 @@ function hamMenuToggle() {
     var menuCheckbox = document.getElementById('hamCheckBox');
 
     if (menuCheckbox.checked == true) {
-        console.log('true');
         document.getElementById("myNav").style.transform = "translate(0%)";
         document.getElementById('navBarUl').style.top = '0';
+        gsap.fromTo('.right-bg', { y: 800 }, { duration: 1, ease: 'power3.in', y: 60 });
+        gsap.fromTo('.tab-content', { y: 800 }, { duration: 1, ease: 'power3.in', y: 0 });
+        gsap.from('.left-tab-title', {
+            duration: 0.5,
+            opacity: 0,
+            delay: 1,
+            stagger: 0.1,
+            ease: 'power2.out',
+            x: 0
+        })
+
     } else {
-        console.log('false');
         document.getElementById("myNav").style.transform = "translate(100%)";
         document.getElementById('navBarUl').style.top = '';
     }
